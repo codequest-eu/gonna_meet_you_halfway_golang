5683501eab7d0326b21a36b4b9ca5a2ac83771c9
Lukasz Solniczek <l.solniczek@gmail.com>
2016-12-16 16:07:41 +0100

    back to google places

--- handler.go
+++ handler.go
@@ -85,7 +85,7 @@ func (h *Handler) acceptMeeting(w http.ResponseWriter, r *http.Request) error {
 		return err
 	}
 	meetingSuggestion.SetCenter(*middlePoint)
-	log.Println(acceptData)
+	log.Println(meetingSuggestion)
 
 	venues, err := meeting.AskForVenues(*middlePoint)
 	if err != nil {
--- meeting/venues.go
+++ meeting/venues.go
@@ -8,6 +8,8 @@ import (
 
 	"encoding/json"
 
+	"log"
+
 	"github.com/codequest-eu/gonna_meet_you_halfway_golang/models"
 )
 
@@ -19,6 +21,7 @@ var (
 
 func AskForVenues(middlePoint models.Position) (*[]models.Venue, error) {
 	url := buildURL(middlePoint)
+	log.Println(url)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
@@ -37,44 +40,44 @@ func AskForVenues(middlePoint models.Position) (*[]models.Venue, error) {
 
 func buildURL(middlePoint models.Position) string {
 	pointParam := fmt.Sprintf("%v,%v", middlePoint.Latitude, middlePoint.Longitude)
-	return "https://api.foursquare.com/v2/venues/search?ll=" + pointParam + "&client_id=" + fsClientID + "&client_secret=" + fsClientSecret + "&v=20161020&m=foursquare&llAcc=100&query=caffee,restaurant,bar&limit=10"
-	// return "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + pointParam + "&radius=200&types=cafe|bar|restaurant&key=" + gpAPIKey
+	// return "https://api.foursquare.com/v2/venues/search?ll=" + pointParam + "&client_id=" + fsClientID + "&client_secret=" + fsClientSecret + "&v=20161020&m=foursquare&llAcc=100&query=caffee,restaurant,bar&limit=10"
+	return "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + pointParam + "&radius=200&types=cafe|bar|restaurant&key=" + gpAPIKey
 }
 
 func getVenues(response map[string]interface{}) *[]models.Venue {
 	var venues []models.Venue
 
-	// respDic := response["results"].([]interface{})
-	// for _, v := range respDic {
-	// 	venue := v.(map[string]interface{})
-	// 	g := venue["geometry"].(map[string]interface{})
-	// 	l := g["location"].(map[string]interface{})
-
-	// 	pos := models.Position{
-	// 		Longitude: l["lng"].(float64),
-	// 		Latitude:  l["lat"].(float64),
-	// 	}
-	// 	venues = append(venues, models.Venue{
-	// 		Name:     venue["name"].(string),
-	// 		Location: pos,
-	// 	})
-	// }
-
-	respDic := response["response"].(map[string]interface{})
-	venuesArr := respDic["venues"].([]interface{})
-
-	for _, v := range venuesArr {
+	respDic := response["results"].([]interface{})
+	for _, v := range respDic {
 		venue := v.(map[string]interface{})
-		loc := venue["location"].(map[string]interface{})
+		g := venue["geometry"].(map[string]interface{})
+		l := g["location"].(map[string]interface{})
 
-		l := models.Position{
-			Longitude: loc["lng"].(float64),
-			Latitude:  loc["lat"].(float64),
+		pos := models.Position{
+			Longitude: l["lng"].(float64),
+			Latitude:  l["lat"].(float64),
 		}
 		venues = append(venues, models.Venue{
 			Name:     venue["name"].(string),
-			Location: l,
+			Location: pos,
 		})
 	}
+
+	// respDic := response["response"].(map[string]interface{})
+	// venuesArr := respDic["venues"].([]interface{})
+
+	// for _, v := range venuesArr {
+	// 	venue := v.(map[string]interface{})
+	// 	loc := venue["location"].(map[string]interface{})
+
+	// 	l := models.Position{
+	// 		Longitude: loc["lng"].(float64),
+	// 		Latitude:  loc["lat"].(float64),
+	// 	}
+	// 	venues = append(venues, models.Venue{
+	// 		Name:     venue["name"].(string),
+	// 		Location: l,
+	// 	})
+	// }
 	return &venues
 }
